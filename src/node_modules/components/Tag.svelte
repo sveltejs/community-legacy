de5daf8dddc0eceab1fe6fa99024ceb07f6a2dc3
Ryan Atkinson <ryanatkn@gmail.com>
2019-10-11 15:03:34 -0500

    add ability to toggle tags to filter resources

---
+++ src/node_modules/components/Tag.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let name;
+  export let selected;
+  export let toggle;
+</script>
+
+<style>
+  /* TODO style this properly, particularly the colors */
+  .tag {
+    padding: 0.2em 0.4em;
+    margin-right: 0.8em;
+    margin-bottom: 0.4em;
+    background-color: rgba(250, 235, 215, 0.4);
+    border: 1px solid transparent;
+    cursor: pointer;
+    line-height: 1.2;
+  }
+  .tag:hover {
+    border-color: rgb(218, 197, 169);
+  }
+  .tag:active {
+    border-color: rgb(173, 158, 138);
+  }
+  .selected {
+    background-color: rgb(250, 235, 215);
+  }
+</style>
+
+<code class="tag" class:selected on:click={() => toggle(name)}>{name}</code>
