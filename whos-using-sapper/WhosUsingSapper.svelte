ff887f9df334b536e5ce37c526ca839b8ea2529e
Kevin Åberg Kultalahti <kevmodrome@users.noreply.github.com>
2020-04-24 19:58:04 +0200

    Revert "Revert "Call to action on 'Who is using Sapper'" (#181)"
    This reverts commit 83d5ca8fff764260a65586d0bba66f1eb0716a31.

--- whos-using-sapper/WhosUsingSapper.svelte
+++ whos-using-sapper/WhosUsingSapper.svelte
@@ -45,4 +45,5 @@
 </style>
 
 <div class="logos">
+<a target="_blank" rel="noopener" href="https://github.com/sveltejs/community/blob/master/whos-using-sapper/WhosUsingSapper.svelte" class="add-yourself"><span>+ your company?</span></a>
 </div>
