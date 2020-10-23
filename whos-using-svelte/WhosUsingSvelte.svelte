c2dc69a03cb019a0bb9c2dcd846fe2f73b369a73
Phuoc Nguyen <me@phuoc.ng>
2020-10-23 09:30:28 +0700

    Add FormValidation logo

--- whos-using-svelte/WhosUsingSvelte.svelte
+++ whos-using-svelte/WhosUsingSvelte.svelte
@@ -83,6 +83,7 @@
 	<a target="_blank" rel="noopener" href="https://www.factry.io/"><img src="organisations/factry.png" alt="Factry logo" loading="lazy"></a>
 	<a target="_blank" rel="noopener" href="https://farmbox.ae/"><img src="organisations/farmbox.svg" alt="Farmbox logo" loading="lazy"></a>
 	<a target="_blank" rel="noopener" href="https://filestar.com/"><img src="organisations/filestar.svg" alt="Filestar logo" loading="lazy"></a>
+	<a target="_blank" rel="noopener" href="https://formvalidation.io"><img src="organisations/formvalidation.svg" alt="FormValidation logo" loading="lazy"></a>
 	<a target="_blank" rel="noopener" href="https://from-now-on.com"><img src="organisations/from-now-on.png" alt="From-Now-On logo" loading="lazy"></a>
 	<a target="_blank" rel="noopener" href="https://frontendbastards.nl" style="background-color: black;"><img src="organisations/frontend_bastards.svg" alt="Frontend Bastards logo" loading="lazy"></a>
 	<a target="_blank" rel="noopener" href="https://fusioncharts.com"><img src="organisations/fusioncharts.svg" alt="FusionCharts logo" loading="lazy"></a>
